4084ce204e6011f2b3b191ceb7722be037aec0f6
tanigross <jgross5@mail.yu.edu>
2025-07-08 20:57:14 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+js
+     const express = require('express');
+     const http = require('http');
+     const { Server } = require('socket.io');
+     const cors = require('cors');
+
+     const app = express();
+     app.use(cors());
+     const server = http.createServer(app);
+     const io = new Server(server, {
+       cors: { origin: '*' }
+     });
+
+     io.on('connection', (socket) => {
+       socket.on('signal', (label) => {
+         io.emit('signal', label);
+       });
+     });
+
+     const PORT = process.env.PORT || 3000;
+     server.listen(PORT, () => {
+       console.log(`Server running on port ${PORT}`);
+     });
